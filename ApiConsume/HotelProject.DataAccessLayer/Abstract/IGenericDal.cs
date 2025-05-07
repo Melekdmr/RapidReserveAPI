32605d2bea38d7489afb5891e94dd5eb10f76ccc
Melekdmr <melekdmrr01@gmail.com>
2025-05-07 21:43:31 +0300

    feat: Add DAL interface definitions for core entities

---
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Abstract
+{
+	public interface IGenericDal<T> where T: class  //It takes the value T from outside but T must be a class
+
+	{
+		void Insert(T t);
+		void Delete(T t);
+		void Update(T t);
+		List<T> GetList();
+		T GetByID(int id);
+	}
+}

308145ab259ae2d29e27f174742cb6750a9a581e
Melekdmr <melekdmrr01@gmail.com>
2025-05-09 19:00:36 +0300

    feat: Add StaffViewModel for listing and displaying staff data

---
+++ Frontend/HotelProject.WebUI/Models/Staff/StaffViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Models.Staff
+{
+	public class StaffViewModel
+	{
+		[Key]
+		public int StaffID { get; set; }
+		public string Name { get; set; }
+		public string Title { get; set; }
+	
+	}
+}

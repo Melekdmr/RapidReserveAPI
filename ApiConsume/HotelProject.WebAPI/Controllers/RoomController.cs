d4781c7810c65dc9be8ad8b642a3c06ba07f1ad9
Melekdmr <melekdmrr01@gmail.com>
2025-05-09 12:57:52 +0300

    Implement RoomController for managing room with CRUD endpoints

---
+++ ApiConsume/HotelProject.WebAPI/Controllers/RoomController.cs
@@ -0,0 +1,57 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RoomController : ControllerBase
+	{
+		
+
+			private readonly IRoomService _roomService;
+
+		public RoomController(IRoomService roomService)
+		{
+			_roomService = roomService;
+		}
+
+		[HttpGet]
+			public IActionResult RoomList()
+			{
+				var values = _roomService.TGetList();
+				return Ok(values);
+			}
+			[HttpPost]
+			public IActionResult AddRoom(Room p)
+			{
+				_roomService.TInsert(p);
+
+				return StatusCode(201);
+			}
+
+			[HttpDelete]
+			public IActionResult DeleteRoom(int id)
+			{
+				var values = _roomService.TGetByID(id);
+				_roomService.TDelete(values);
+				return Ok();
+			}
+			[HttpPut]
+			public IActionResult UpdateRoom(Room room)
+			{
+				_roomService.TUpdate(room);
+				return Ok();
+			}
+			[HttpGet("{id}")]
+			public IActionResult GetRoom(int id)
+			{
+				var values = _roomService.TGetByID(id);
+				return Ok(values);
+			}
+		}
+	}
+
+

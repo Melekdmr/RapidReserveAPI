c30bc951dac0de03c152f9a32a979aad2e313350
Melekdmr <melekdmrr01@gmail.com>
2025-05-19 12:53:06 +0300

    Create AutoMapper profile for Room and DTOs

---
+++ ApiConsume/HotelProject.WebAPI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebAPI.Mapping
+{
+	public class AutoMapperConfig: Profile //Profile sınıfından kalıtım alıyor. Bu, AutoMapper konfigürasyonu yapmanı sağlıyor. eşleştirme kurallarını barındırır
+	{
+		public AutoMapperConfig()//Bu sınıfın kurucu metodudur (constructor). Uygulama başlatıldığında çalışır. İçerisinde haritalama kuralları tanımlanır.
+
+		{
+			CreateMap<RoomAddDto, Room>();//oomAddDto → Room sınıfına veri dönüştürmesini tanımlar.
+			CreateMap<Room, RoomAddDto>();
+
+			CreateMap<UpdateRoomDto, Room>().ReverseMap(); //.ReverseMap() sayesinde  çift yönlü dönüşüm yapılabilir.
+
+		}
+	}
+}

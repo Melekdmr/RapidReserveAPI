cc3b87439fd2cebd3cbfcce9b0515c6cc412168e
Melekdmr <melekdmrr01@gmail.com>
2025-05-23 15:15:41 +0300

    Configure DbContext and Identity services in Program.cs

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,7 +1,12 @@
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddHttpClient(); //UI katmanýnda API'ye istek göndermek için HttpClient kullanýlýr. Bu servis, dýþ API'lere HTTP istekleri yapmanýzý saðlar.
 
 builder.Services.AddAutoMapper(typeof(Program));

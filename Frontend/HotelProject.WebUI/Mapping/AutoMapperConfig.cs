aa8cc5de57924a7989be849685554b8ecd24149f
Melekdmr <melekdmrr01@gmail.com>
2025-05-24 22:01:23 +0300

    Add AutoMapper mappings for ResultTestimonialDto, ResultStaffDto, and CreateSubscribeDto

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -4,6 +4,9 @@ using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
 // Burayı düzelt
 
 namespace HotelProject.WebUI.Mapping
@@ -15,10 +18,20 @@ namespace HotelProject.WebUI.Mapping
 			CreateMap<ResultServiceDto, Service>().ReverseMap();
 			CreateMap<UpdateServiceDto, Service>().ReverseMap();
 			CreateMap<CreateServiceDto, Service>().ReverseMap();
+
 			CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
 			CreateMap<LoginUserDto, AppUser>().ReverseMap();
+
 			CreateMap<ResultAboutDto, About>().ReverseMap();
 			CreateMap<UpdateAboutDto, About>().ReverseMap();
+
+			CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+			CreateMap<ResultStaffDto, Staff>().ReverseMap();
+
+			CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
+		
+
+
 		}
 	}
 }

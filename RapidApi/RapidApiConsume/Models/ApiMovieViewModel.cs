84d3fa47db96191c8a6c4565927060d3b1488e1c
Melekdmr <melekdmrr01@gmail.com>
2025-05-25 18:28:08 +0300

    Create ApiMovieViewModel to map movie data returned from RapidAPI

---
+++ RapidApi/RapidApiConsume/Models/ApiMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RapidApiConsume.Models
+{
+	public class ApiMovieViewModel
+	{
+		public double averageRating { get; set; }
+		public string primaryTitle  { get; set; }
+		public string description { get; set; }
+		public string trailer { get; set; }
+		
+	}
+}

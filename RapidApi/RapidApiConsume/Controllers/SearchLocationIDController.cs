53a598915c3a6316a4266c88d1a100fa5cb431ed
Melekdmr <melekdmrr01@gmail.com>
2025-05-28 20:05:27 +0300

    Create SearchLocationIDController to fetch location data from Booking.com API

---
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+	public class SearchLocationIDController : Controller
+	{
+		public async Task<IActionResult> Index(string cityName)
+		{
+			if (!string.IsNullOrEmpty(cityName))
+			{
+				List<BookingApiLocationSerachViewModel> models = new List<BookingApiLocationSerachViewModel>();
+				var client = new HttpClient();
+				var request = new HttpRequestMessage
+				{
+					Method = HttpMethod.Get,
+					RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name={cityName}"),
+					Headers =
+	{
+		{ "x-rapidapi-key", "d9c339f828msha13543048cb994ap1327d7jsnd28779ad6013" },
+		{ "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+	},
+				};
+				using (var response = await client.SendAsync(request))
+				{
+					response.EnsureSuccessStatusCode();
+					var body = await response.Content.ReadAsStringAsync();
+					models = JsonConvert.DeserializeObject<List<BookingApiLocationSerachViewModel>>(body);
+					return View(models.Take(1).ToList());
+				}
+			}
+			else
+			{
+				List<BookingApiLocationSerachViewModel> models = new List<BookingApiLocationSerachViewModel>();
+				var client = new HttpClient();
+				var request = new HttpRequestMessage
+				{
+					Method = HttpMethod.Get,
+					RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name=Paris"),
+					Headers =
+	{
+		{ "x-rapidapi-key", "d9c339f828msha13543048cb994ap1327d7jsnd28779ad6013" },
+		{ "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+	},
+				};
+				using (var response = await client.SendAsync(request))
+				{
+					response.EnsureSuccessStatusCode();
+					var body = await response.Content.ReadAsStringAsync();
+					models = JsonConvert.DeserializeObject<List<BookingApiLocationSerachViewModel>>(body);
+					return View(models.Take(1).ToList());
+				}
+			}
+		
+		}
+	}
+}
+

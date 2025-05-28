9fa45f7fe6c109d054f60ec97e829921f38a7bfd
Melekdmr <melekdmrr01@gmail.com>
2025-05-28 14:24:43 +0300

    feat: Implemented BookingController to fetch hotel data from RapidAPI

---
+++ RapidApi/RapidApiConsume/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+	public class BookingController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&adults_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&checkout_date=2025-06-05&dest_type=city&page_number=0&units=metric&order_by=popularity&room_number=1&checkin_date=2025-05-29&filter_by_currency=EUR&dest_id=-1456928&locale=en-gb&include_adjacency=true"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "d9c339f828msha13543048cb994ap1327d7jsnd28779ad6013" },
+		{ "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<BookingApiModel>(body);
+				return View(values.results.ToList());
+
+			}
+		}
+	}
+}

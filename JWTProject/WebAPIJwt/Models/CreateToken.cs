de023a3ec6344a2eb74b2eb5de896def20698138
Melekdmr <melekdmrr01@gmail.com>
2025-05-30 18:04:16 +0300

    Add TokenCreate method to generate JWT with symmetric security key and signing credentials

---
+++ JWTProject/WebAPIJwt/Models/CreateToken.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebAPIJwt.Models
+{
+	public class CreateToken
+	{
+		public string TokenCreate()
+		{
+		 var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi_secret_key_32chars_minimum");
+			SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+			SigningCredentials signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			JwtSecurityToken token = new JwtSecurityToken(
+				issuer: "http://localhost",
+				audience: "http://localhost",
+				claims: null, // Bu parametreyi ekleyin
+				notBefore: DateTime.Now,
+				expires: DateTime.Now.AddSeconds(30),
+				signingCredentials: signingCredentials
+			);
+			JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+			 return handler.WriteToken(token);
+		}
+	}
+}

b6c2ef8dec4665eae49883f8cfbf0f1fbf886519
Melekdmr <melekdmrr01@gmail.com>
2025-05-30 18:05:37 +0300

    Add DefaultController with Test action to generate JWT and Test2 action secured with [Authorize]

---
+++ JWTProject/WebAPIJwt/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIJwt.Models;
+
+namespace WebAPIJwt.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet("[action]")]
+		public IActionResult Test()
+		{
+			return Ok(new CreateToken().TokenCreate()); //hata vermemek için Ok
+		}
+		[Authorize(AuthenticationSchemes ="Bearer")]
+		[HttpGet("[action]")]
+		public IActionResult Test2()
+		{
+			return Ok("Yetkilendirme başarılı!");
+		}
+
+	}
+}

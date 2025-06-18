9397c1810ac712937e3defc57838df8be9a93dd8
Melekdmr <melekdmrr01@gmail.com>
2025-06-18 21:29:54 +0300

    Implement BookingByCityController to fetch hotel data by cityId using RapidAPI

--- RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
+++ RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
@@ -15,13 +15,14 @@ namespace RapidApiConsume.Controllers
 				var request = new HttpRequestMessage
 				{
 					Method = HttpMethod.Get,
-					RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&adults_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&checkout_date=2025-06-05&dest_type=city&page_number=0&units=metric&order_by=popularity&room_number=1&checkin_date=2025-05-29&filter_by_currency=EUR&dest_id={cityId}&locale=en-gb&include_adjacency=true"),
+					RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&adults_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&checkout_date=2025-10-15&dest_type=city&page_number=0&units=metric&order_by=popularity&room_number=1&checkin_date=2025-10-14&filter_by_currency=AED&dest_id={cityId}&locale=en-gb&include_adjacency=true"),
 					Headers =
 	{
 		{ "x-rapidapi-key", "d9c339f828msha13543048cb994ap1327d7jsnd28779ad6013" },
 		{ "x-rapidapi-host", "booking-com.p.rapidapi.com" },
 	},
 				};
+				
 				using (var response = await client.SendAsync(request))
 				{
 					response.EnsureSuccessStatusCode();
@@ -39,7 +40,7 @@ namespace RapidApiConsume.Controllers
 				var request = new HttpRequestMessage
 				{
 					Method = HttpMethod.Get,
-					RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&adults_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&checkout_date=2025-06-05&dest_type=city&page_number=0&units=metric&order_by=popularity&room_number=1&checkin_date=2025-05-29&filter_by_currency=EUR&dest_id=-1456928&locale=en-gb&include_adjacency=true"),
+					RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&adults_number=2&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&checkout_date=2025-10-15&dest_type=city&page_number=0&units=metric&order_by=popularity&room_number=1&checkin_date=2025-10-14&filter_by_currency=AED&dest_id=-1456928&locale=en-gb&include_adjacency=true"),
 					Headers =
 	{
 		{ "x-rapidapi-key", "d9c339f828msha13543048cb994ap1327d7jsnd28779ad6013" },
@@ -60,3 +61,5 @@ namespace RapidApiConsume.Controllers
 		}
 	}
 }
+
+

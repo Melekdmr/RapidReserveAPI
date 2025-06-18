beb9a2de1697d9b82d9009dfb66ce3ad07a1c57b
Melekdmr <melekdmrr01@gmail.com>
2025-06-18 21:28:31 +0300

    Add role-based authorization and token generation endpoints

--- JWTProject/WebAPIJwt/Controllers/DefaultController.cs
+++ JWTProject/WebAPIJwt/Controllers/DefaultController.cs
@@ -9,17 +9,44 @@ namespace WebAPIJwt.Controllers
 	[ApiController]
 	public class DefaultController : ControllerBase
 	{
-		[HttpGet("[action]")]
-		public IActionResult Test()
+		// GET api/default/test
+		// Bu endpoint, yeni bir JWT token oluşturur ve döner.
+		// Token oluşturma işlemi CreateToken sınıfındaki TokenCreate metoduyla yapılır.
+		// Ok() ile döndürülmesi, HTTP 200 OK ve token stringi anlamına gelir.
+		[HttpGet("[action]")] //"[action]": Route URL'sinde metodun adının kullanılacağını belirtir. metodun adı ile değiştirilir, yani Test veya Test2.
+		public IActionResult TokenOlustur()
 		{
-			return Ok(new CreateToken().TokenCreate()); //hata vermemek için Ok
+			return Ok(new CreateToken().TokenCreate()); // Token döndürülür
 		}
-		[Authorize(AuthenticationSchemes ="Bearer")]
+
+
+
+		[HttpGet("[action]")] 
+		public IActionResult AdminTokenOlustur()
+		{
+			return Ok(new CreateToken().TokenCreateAdmin()); // Token döndürülür
+		}
+
+
+
+		// GET api/default/test2
+		// Bu endpoint 'Bearer' tipi JWT token ile yetkilendirilmiştir.
+		// Yani bu metoda erişmek için geçerli bir JWT token ile Authorization header gönderilmelidir.
+		// Token doğrulanırsa "Yetkilendirme başarılı!" mesajı döner.
+		[Authorize(AuthenticationSchemes = "Bearer")]
 		[HttpGet("[action]")]
 		public IActionResult Test2()
 		{
-			return Ok("Yetkilendirme başarılı!");
+			return Ok("Yetkilendirme başarılı!"); // Yetkilendirme başarılı ise bu cevap döner
 		}
 
+
+
+		[Authorize(AuthenticationSchemes = "Bearer" ,Roles ="Admin,Visitor")]
+		[HttpGet("[action]")]
+		public IActionResult Test3()  //Erişim yetkisi yoksa 403 hatası alırız
+		{
+			return Ok(" Rol kapsamlı Yetkilendirme başarılı!"); // Yetkilendirme başarılı ise bu cevap döner
+		}
 	}
 }

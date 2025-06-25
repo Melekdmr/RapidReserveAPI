75dc486de022eaa1dc81c3a6bc461f3547ae4928
Melekdmr <melekdmrr01@gmail.com>
2025-06-25 22:21:32 +0300

    feat(api): add ContactController with POST endpoint to add new contacts

---
+++ ApiConsume/HotelProject.WebAPI/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+
+
+
+namespace HotelProject.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactController : ControllerBase
+	{
+		private readonly IContactService _contactService;
+
+		public ContactController(IContactService contactService)
+		{
+			_contactService = contactService;
+		}
+		[HttpPost]
+		public IActionResult AddContect(Contact p)
+		{
+			p.Date = Convert.ToDateTime(DateTime.Now.ToString());
+			_contactService.TInsert(p);
+
+			return StatusCode(201);
+		}
+
+	}
+}

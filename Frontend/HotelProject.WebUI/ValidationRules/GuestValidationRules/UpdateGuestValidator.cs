3b925908eb3e7752d7100339ad15035192f40829
Melekdmr <melekdmrr01@gmail.com>
2025-07-04 18:42:46 +0300

    feat: Added UpdateGuestValidator with validation rules for Name, SurName, and City fields

---
+++ Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/UpdateGuestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using HotelProject.WebUI.Dtos.GuestDto;
+
+namespace HotelProject.WebUI.ValidationRules.GuestValidationRules
+{
+	public class UpdateGuestValidator:AbstractValidator<UpdateGuestDto> 
+	{
+		public UpdateGuestValidator()
+		{
+
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez.");
+			RuleFor(x => x.SurName).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+			RuleFor(x => x.City).NotEmpty().WithMessage("Şehir alanı boş geçilemez");
+			RuleFor(x => x.Name).MinimumLength(3).WithMessage("En az 3 karakter girmelisiniz");
+			RuleFor(x => x.SurName).MinimumLength(3).WithMessage("En az 3 karakter girmelisiniz");
+			RuleFor(x => x.City).MinimumLength(3).WithMessage("En az 3 karakter girmelisiniz");
+			RuleFor(x => x.Name).MaximumLength(15).WithMessage("En fazla 15 karakter girmelisiniz");
+			RuleFor(x => x.SurName).MaximumLength(15).WithMessage("En fazla 15 karakter girmelisiniz");
+			RuleFor(x => x.City).MaximumLength(20).WithMessage("En fazla 20 karakter girmelisiniz");
+
+		}
+	}
+}

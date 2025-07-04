f3ec31d034dd5a08453c1689b1395e8168e91530
Melekdmr <melekdmrr01@gmail.com>
2025-07-04 13:02:02 +0300

    Register IGuestDal and IGuestService implementations in DI container in Program.cs

--- ApiConsume/HotelProject.WebAPI/Program.cs
+++ ApiConsume/HotelProject.WebAPI/Program.cs
@@ -33,6 +33,8 @@ builder.Services.AddScoped<IBookingDal, EfBookingDal>();
 builder.Services.AddScoped<IBookingService, BookingManager>();
 builder.Services.AddScoped<IContactDal, EfContactDal>();
 builder.Services.AddScoped<IContactService, ContactManager>();
+builder.Services.AddScoped<IGuestDal, EfGuestDal>();
+builder.Services.AddScoped<IGuestService, GuestManager>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 //builder.Services.AddScoped

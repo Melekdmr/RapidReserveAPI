9b7c4068496d8120b71d82b8209c5e9364f3e340
Melekdmr <melekdmrr01@gmail.com>
2025-07-04 13:01:04 +0300

    The EfGuestDal class inherits from GenericRepository and implements the IGuestDal interface.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfGuestDal.cs
@@ -0,0 +1,26 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+	public class EfGuestDal : GenericRepository<Guest>, IGuestDal
+	{
+		public EfGuestDal(Context context) : base(context)
+		/*
+		✅ Context context: Entity Framework DbContext nesnesi
+		✅ : base(context): Üst sınıfın (GenericRepository) constructor'ına context'i gönderir
+		✅ Dependency Injection ile context dışarıdan alınır
+		✅ Bu sayede test edilebilir ve gevşek bağlı bir yapı oluşur
+		*/
+		{
+		}
+	}
+}
+

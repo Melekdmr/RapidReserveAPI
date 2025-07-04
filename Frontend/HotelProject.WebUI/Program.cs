069a33761d26e615f99130d294eaa2bddb310787
Melekdmr <melekdmrr01@gmail.com>
2025-07-04 18:41:33 +0300

    Added FluentValidation configuration with validators registered for CreateGuestDto and UpdateGuestDto, enabling automatic and client-side validatio

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,10 +1,28 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.GuestDto;
+using HotelProject.WebUI.ValidationRules.GuestValidationRules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+// FluentValidation konfigürasyonu
+builder.Services.AddFluentValidationAutoValidation(config =>
+{
+    config.DisableDataAnnotationsValidation = true; // Önemli: DataAnnotations'ý kapat aksi halde fluent validation çalýþmaz mesjalar görünmez
+});
+builder.Services.AddFluentValidationClientsideAdapters();//Formda kullanýcý yazmaya baþladýðýnda anýnda validation yapar
+//Sayfa yenilenmeden hata mesajlarý gösterir
+
+// Validator'ý kaydet
+builder.Services.AddScoped<IValidator<CreateGuestDto>, CreateGuestValidator>();
+builder.Services.AddScoped<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
+
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddHttpClient(); //UI katmanýnda API'ye istek göndermek için HttpClient kullanýlýr. Bu servis, dýþ API'lere HTTP istekleri yapmanýzý saðlar.

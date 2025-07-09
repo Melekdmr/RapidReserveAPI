1998f9253eb9cf40a94c34112b89779640a500e8
Melekdmr <melekdmrr01@gmail.com>
2025-07-09 18:08:01 +0300

    Add migration to create SendMessages table

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20250709150553_mig_add_sendmessage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_sendmessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SendMessages",
+                columns: table => new
+                {
+                    SendMessageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiverName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SendMessages", x => x.SendMessageId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SendMessages");
+        }
+    }
+}

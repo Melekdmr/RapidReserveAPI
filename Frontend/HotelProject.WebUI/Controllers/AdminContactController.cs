00cfe3e9e1e73a7543edd1d031908682bba76909
Melekdmr <melekdmrr01@gmail.com>
2025-07-14 13:08:21 +0300

    Added MessageDetailsByInbox action to AdminContactController for displaying inbox message details.

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -31,8 +31,24 @@ namespace HotelProject.WebUI.Controllers
 			}
 			return View();  
 		}
-	
-	
+		public async Task<IActionResult> SendBox()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var responseMessage = await client.GetAsync("http://localhost:5035/api/SendMessage");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+
+				var values = JsonConvert.DeserializeObject<List<ResultSendBoxDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+
+
 		[HttpGet]
 		public IActionResult AddSendMessage()
 		{
@@ -63,6 +79,33 @@ namespace HotelProject.WebUI.Controllers
 		{
 			return PartialView();
 		}
-	
+	public async Task< IActionResult> MessageDetailsSendBox(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"http://localhost:5035/api/SendMessage/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<GetMessageByIdDto>(jsonData);
+				return View(values);
+			}
+			return View();
+
+
+		}
+		public async Task<IActionResult> MessageDetailsByInbox(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"http://localhost:5035/api/Contact/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<InboxContactDto>(jsonData);
+				return View(values);
+			}
+			return View();
+
+
+		}
 	}
 }

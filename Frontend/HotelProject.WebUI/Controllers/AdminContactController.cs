07c545df360abfa27e5c76db128bd707038e4382
Melekdmr <melekdmrr01@gmail.com>
2025-07-18 22:36:02 +0300

    Fetch contact and send message counts from API and pass to ViewBag in Inbox action

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -18,17 +18,34 @@ namespace HotelProject.WebUI.Controllers
 
 		public async Task<IActionResult> Inbox()
 		{
-			var client = _httpClientFactory.CreateClient();  
 
-			var responseMessage = await client.GetAsync("http://localhost:5035/api/Contact");
+			var client2 = _httpClientFactory.CreateClient();
+
+			var responseMessage2 = await client2.GetAsync("http://localhost:5035/api/Contact/GetContactCount");
+
+			var client3 = _httpClientFactory.CreateClient();
+
+			var responseMessage3 = await client3.GetAsync("http://localhost:5035/api/SendMessage/GetSendMessageCount");
+
+			var client = _httpClientFactory.CreateClient();  
+            var responseMessage = await client.GetAsync("http://localhost:5035/api/Contact");
+
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+               var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
 
-				
-				var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+				var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+				ViewBag.contactCount = jsonData2;
+
+
+				var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+				ViewBag.sendMessageCount = jsonData3;
+
+
 				return View(values); 
 			}
+
 			return View();  
 		}
 		public async Task<IActionResult> SendBox()
@@ -71,10 +88,16 @@ namespace HotelProject.WebUI.Controllers
 			return View(model);
 
 		}
-		public PartialViewResult SideBarAdminContactPartial()
+		public async Task< PartialViewResult> SideBarAdminContactPartial()
 		{
+			
+		
+			
+		
 			return PartialView();
 		}
+		
+		
 		public PartialViewResult SideBarAdminContactCaetgoryPartial()
 		{
 			return PartialView();
@@ -107,5 +130,6 @@ namespace HotelProject.WebUI.Controllers
 
 
 		}
+		
 	}
 }

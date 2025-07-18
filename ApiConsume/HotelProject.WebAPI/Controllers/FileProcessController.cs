60eb46a5d5a783218939d880e32bcf8e8f342929
Melekdmr <melekdmrr01@gmail.com>
2025-07-18 17:42:57 +0300

    add FileProcessController to handle file uploads

---
+++ ApiConsume/HotelProject.WebAPI/Controllers/FileProcessController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FileProcessController : ControllerBase
+	{
+		[HttpPost]
+		public async Task<IActionResult> UploadFile(IFormFile file)
+		{
+			var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "files", fileName);
+
+			using (var stream = new FileStream(path, FileMode.Create))
+			{
+				await file.CopyToAsync(stream);
+			}
+
+			return Created("", file);
+		}
+	}
+}

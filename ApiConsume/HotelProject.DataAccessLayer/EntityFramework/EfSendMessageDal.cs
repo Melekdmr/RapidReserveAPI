967da864984144e801a940bf76255fc1004acbb8
Melekdmr <melekdmrr01@gmail.com>
2025-07-18 22:34:50 +0300

    Implement GetSendMessageCount method in SendMessageDal using EF context

--- ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfSendMessageDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfSendMessageDal.cs
@@ -14,6 +14,13 @@ public class EfSendMessageDal : GenericRepository<SendMessage>, ISendMessageDal
 	{
 	public EfSendMessageDal(Context context) : base(context)
 		{
+				
+	}
+
+		public int GetSendMessageCount()
+		{
+			var context = new Context();
+			return context.SendMessages.Count();
 		}
 	}
 }

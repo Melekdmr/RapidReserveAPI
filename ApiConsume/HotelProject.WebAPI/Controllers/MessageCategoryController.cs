a4a5cf780d8d9185a6712c5c12607a2b4250cdae
Melekdmr <melekdmrr01@gmail.com>
2025-07-20 14:26:43 +0300

    implemented CRUD API endpoints for MessageCategory in MessageCategoryController

---
+++ ApiConsume/HotelProject.WebAPI/Controllers/MessageCategoryController.cs
@@ -0,0 +1,56 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using HotelProject.BusinessLayer.Abstract;
+
+
+namespace HotelProject.WebUI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MessageCategoryController : ControllerBase
+	{
+		private readonly IMessageCategoryService _messageCategoryService;
+
+		public MessageCategoryController(IMessageCategoryService messageCategoryService)
+		{
+			_messageCategoryService = messageCategoryService;
+		}
+
+		[HttpGet]
+		public IActionResult MessageCategoryList()
+		{
+			var values = _messageCategoryService.TGetList();
+			return Ok(values);
+		}
+		[HttpPost]
+		public IActionResult AddMessageCategory(MessageCategory p)
+		{
+			_messageCategoryService.TInsert(p);
+
+			return StatusCode(201);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteMessageCategory(int id)
+		{
+			var values = _messageCategoryService.TGetByID(id);
+			_messageCategoryService.TDelete(values);
+			return Ok();
+		}
+		[HttpPut]
+		public IActionResult UpdateMessageCategory(MessageCategory messageCategory)
+		{
+			_messageCategoryService.TUpdate(messageCategory);
+			return Ok();
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetMessageCategory(int id)
+		{
+			var values = _messageCategoryService.TGetByID(id);
+			return Ok(values);
+		}
+	}
+}
+

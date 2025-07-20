0bf93760dfa871a738877de3ad8d6684f973e1d4
Melekdmr <melekdmrr01@gmail.com>
2025-07-20 14:25:50 +0300

    fix: added nullable (?) to Subject, Message, and MessageCategory properties in Contact entity

--- ApiConsume/HotelProject.EntityLayer/Concrete/Contact.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Contact.cs
@@ -12,8 +12,10 @@ public class Contact
 		public string Name { get; set; }
 		public string Mail { get; set; }
 	
-		public string Subject { get; set; }
-		public string Message { get; set; }
+		public string? Subject { get; set; }
+		public string? Message { get; set; }
 		public DateTime Date { get; set; }
+		public int MessageCategoryID { get; set; }
+		public MessageCategory? MessageCategory { get; set; }
 	}
 }

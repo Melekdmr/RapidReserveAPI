a79b705b689f0b810009c3e6a0451cfd177109d9
Melekdmr <melekdmrr01@gmail.com>
2025-09-10 12:02:48 +0300

    Add WorkLocation entity and establish one-to-many relationship with AppUser

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20250903171611_mig_reserved.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_reserved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WorkLocationName",
+                table: "WorkLocations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkLocationName",
+                table: "WorkLocations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}

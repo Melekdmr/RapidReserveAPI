8287ce3ed6edd752745a11bda1b12ac4956b8058
Melekdmr <melekdmrr01@gmail.com>
2025-09-10 12:10:47 +0300

    Add CreateNewUserDto with validation attributes for user registration

--- Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -27,6 +27,7 @@ namespace HotelProject.WebUI.Dtos.RegisterDto
 		[Required(ErrorMessage = "Şifre Tekrar alanı gereklidir")]
 		[Compare("Password", ErrorMessage = "Şifreler uyuşmuyor")]
 		public string ConfirmPassword { get; set; }
+	
 	}
 }
 

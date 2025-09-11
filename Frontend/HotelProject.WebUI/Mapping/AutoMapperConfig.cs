52a5f468c9b4db93d85ea4a75868ddad3f048045
Melekdmr <melekdmrr01@gmail.com>
2025-09-11 16:34:57 +0300

    feat(mapping): add AutoMapper profile for ResultAppUserDto <-> AppUser

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -36,7 +36,7 @@ namespace HotelProject.WebUI.Mapping
 			CreateMap<ApprovedReservationDto, Booking>().ReverseMap();
 			CreateMap<CreateGuestDto, Guest>().ReverseMap();
 			CreateMap<UpdateGuestDto, Guest>().ReverseMap();
-		
+
 			CreateMap<ResultAppUserDto, AppUser>().ReverseMap();
 			
 

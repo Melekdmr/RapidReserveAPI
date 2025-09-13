421440bcce637a7537bb44d2e7ef45429ead5ff0
Melekdmr <melekdmrr01@gmail.com>
2025-09-13 15:26:07 +0300

    Use DI for Context in EfAppUserDal and implement UserListWithWorkLocation with eager loading

--- ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfAppUserDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfAppUserDal.cs
@@ -13,13 +13,22 @@ namespace HotelProject.DataAccessLayer.EntityFramework
 {
 	public class EfAppUserDal : GenericRepository<AppUser>, IAppUserDal
 	{
+		private readonly Context _context;
+		/*Context: EF Core’un veritabanı bağlantısını yöneten sınıf.
+
+         Dependency Injection (DI) ile dışarıdan Context alınıyor.
+
+         base(context): GenericRepository’nin constructor’ına context gönderiliyor, 
+		yani temel CRUD işlemleri için de aynı context kullanılabiliyor.
+
+        _context = context;: Bu sınıfta kullanılmak üzere context saklanıyor.*/
 		public EfAppUserDal(Context context) : base(context)
 		{
+			_context = context;
 		}
 		public List<AppUser> UserListWithWorkLocation()
 		{
-			var context = new Context();
-			return context.Users.Include(x => x.WorkLocation).ToList();
+			return _context.Users.Include(x => x.WorkLocation).ToList(); /*Kullanıcıların bağlı olduğu WorkLocation tablosunu da sorguya dahil et (Eager Loading).*/
 		}
 	}
 }

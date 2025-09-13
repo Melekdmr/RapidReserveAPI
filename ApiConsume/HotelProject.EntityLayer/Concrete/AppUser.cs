2b3ea2e841c799dcef5060f07b1e86004ce6ea79
Melekdmr <melekdmrr01@gmail.com>
2025-09-13 15:24:34 +0300

    Add Country and Gender properties to AppUser entity to store user's country and gender information

--- ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -14,6 +14,7 @@ namespace HotelProject.EntityLayer.Concrete
 		public string? City { get; set; }
 		public string? ImageUrl { get; set; }
 		public string? Country { get; set; }
+		public string? Gender { get; set; }
 		public string WorkDepartment { get; set; }
 		public int WorkLocationId { get; set; }
 		public WorkLocation WorkLocation { get; set; } //Kullanıcı ile WorkLocation arasında navigation property (ilişkiyi temsil eden alan).

f06907cf78e24d31852552926c826dcb34b9c2ed
Melekdmr <melekdmrr01@gmail.com>
2025-09-18 19:52:56 +0300

    feat(dto): add ResultInstagramFollowersDto for Instagram follower and following count

---
+++ Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultInstagramFollowersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.WebUI.Dtos.FollowersDto
+{
+	public class ResultInstagramFollowersDto
+	{
+
+		public int follower_count { get; set; }
+		public int following_count { get; set; }
+
+	}
+}

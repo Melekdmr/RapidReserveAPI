a975b5da0274db54d8eb2d4279ae105936635269
Melekdmr <melekdmrr01@gmail.com>
2025-09-30 13:42:07 +0300

    feat: Add UpdateBooking GET and POST actions with API integration

--- Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -108,6 +108,41 @@ namespace HotelProject.WebUI.Controllers
 			return View();
 		}
 
+		[HttpGet]
+		 public async Task<IActionResult> UpdateBooking(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"http://localhost:5035/api/Booking/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateBookingDto>(jsonData);
+				return View(values);
+
+			}
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateBooking(UpdateBookingDto updateBookingDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(updateBookingDto);
+			}
+
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateBookingDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+			var responseMessage = await client.PutAsync($"http://localhost:5035/api/Booking/{updateBookingDto.BookingID}", stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View(updateBookingDto);
+		}
 
 
 		
